c932343c67404808cd0c38751a555697c6fed7df
Raj Kiran Singh <rajkiran.singh@rudrasofttech.com>
2020-08-17 13:06:24 +0530

    Add project files.

---
+++ Sketchy/Sketchy/MainPage.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using System.Reflection;
+
+namespace Sketchy
+{
+    public partial class MainPage : ContentPage
+    {
+        Color ChosenColor = Color.Black;
+        Dictionary<long, FingerPaintPolyline> inProgressPolylines = new Dictionary<long, FingerPaintPolyline>();
+        List<FingerPaintPolyline> completedPolylines = new List<FingerPaintPolyline>();
+
+        SKPaint paint = new SKPaint
+        {
+            Style = SKPaintStyle.Stroke,
+            StrokeCap = SKStrokeCap.Round,
+            StrokeJoin = SKStrokeJoin.Round
+        };
+        public MainPage()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        void OnClearButtonClicked(object sender, EventArgs args)
+        {
+            completedPolylines.Clear();
+            canvasView.InvalidateSurface();
+        }
+
+        void OnTouchEffectAction(object sender, TouchActionEventArgs args)
+        {
+            switch (args.Type)
+            {
+                case TouchActionType.Pressed:
+                    if (!inProgressPolylines.ContainsKey(args.Id))
+                    {
+                        Color strokeColor = ChosenColor; //(Color)typeof(Color).GetRuntimeField(colorPicker.Items[colorPicker.SelectedIndex]).GetValue(null);
+                        float strokeWidth = ConvertToPixel(8/*new float[] { 1, 2, 5, 10, 20 }[widthPicker.SelectedIndex]*/);
+
+                        FingerPaintPolyline polyline = new FingerPaintPolyline
+                        {
+                            StrokeColor = strokeColor,
+                            StrokeWidth = strokeWidth
+                        };
+                        polyline.Path.MoveTo(ConvertToPixel(args.Location));
+
+                        inProgressPolylines.Add(args.Id, polyline);
+                        canvasView.InvalidateSurface();
+                    }
+                    break;
+
+                case TouchActionType.Moved:
+                    if (inProgressPolylines.ContainsKey(args.Id))
+                    {
+                        FingerPaintPolyline polyline = inProgressPolylines[args.Id];
+                        polyline.Path.LineTo(ConvertToPixel(args.Location));
+                        canvasView.InvalidateSurface();
+                    }
+                    break;
+
+                case TouchActionType.Released:
+                    if (inProgressPolylines.ContainsKey(args.Id))
+                    {
+                        completedPolylines.Add(inProgressPolylines[args.Id]);
+                        inProgressPolylines.Remove(args.Id);
+                        canvasView.InvalidateSurface();
+                    }
+                    break;
+
+                case TouchActionType.Cancelled:
+                    if (inProgressPolylines.ContainsKey(args.Id))
+                    {
+                        inProgressPolylines.Remove(args.Id);
+                        canvasView.InvalidateSurface();
+                    }
+                    break;
+            }
+        }
+
+        void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            SKCanvas canvas = args.Surface.Canvas;
+            canvas.Clear();
+
+            foreach (FingerPaintPolyline polyline in completedPolylines)
+            {
+                paint.Color = polyline.StrokeColor.ToSKColor();
+                paint.StrokeWidth = polyline.StrokeWidth;
+                canvas.DrawPath(polyline.Path, paint);
+            }
+
+            foreach (FingerPaintPolyline polyline in inProgressPolylines.Values)
+            {
+                paint.Color = polyline.StrokeColor.ToSKColor();
+                paint.StrokeWidth = polyline.StrokeWidth;
+                canvas.DrawPath(polyline.Path, paint);
+            }
+        }
+
+        SKPoint ConvertToPixel(Point pt)
+        {
+            return new SKPoint((float)(canvasView.CanvasSize.Width * pt.X / canvasView.Width),
+                               (float)(canvasView.CanvasSize.Height * pt.Y / canvasView.Height));
+        }
+
+        float ConvertToPixel(float fl)
+        {
+            return (float)(canvasView.CanvasSize.Width * fl / canvasView.Width);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            foreach(View v in ColorStack.Children)
+            {
+                (v as Button).BorderColor = Color.Transparent;
+            }
+
+            ChosenColor = Color.FromHex((sender as Button).CommandParameter.ToString());
+            (sender as Button).BorderColor = Color.Blue;
+        }
+    }
+
+    class FingerPaintPolyline
+    {
+        public FingerPaintPolyline()
+        {
+            Path = new SKPath();
+        }
+
+        public SKPath Path { set; get; }
+
+        public Color StrokeColor { set; get; }
+
+        public float StrokeWidth { set; get; }
+    }
+}
